29f06862a44a18d524a635f3daafd0acf96e05f7
joao-barroso-1190754 <119754@isep.ipp.pt>
2026-06-17 14:44:56 +0100

    criacao config php

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define('APP_NAME', 'Clinica Madureira Loureiro');
+define('APP_VERSION', '1.0.0');
+define('APP_COPYRIGHT', '© 2026 ISEP');
+?>
---
+++ config/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'vsgate-s1.dei.isep.ipp.pt:10464';
+$db_name = 'db1190754';
+$username = '1190754'; 
+$password = 'barroso_754'; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
